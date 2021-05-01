267c8bd3031d941250bd0ce9de912398169a438e
JPRMohnish <47176966+JPRMohnish@users.noreply.github.com>
2021-05-01 22:05:20 +0530

    Plasma Front End v 1.0 FInaal Commit.
    Plasma Front End v 1.0 FInaal Commit.

---
+++ README.md
@@ -0,0 +1,24 @@
+### Front End for PlasmaDonor Apis
+
+Huge tribute and shout out to font awesome , Creative tim and neucleo icons for their awesom frontend templates. and icons.
+Some folders and css files are missing which were subjected to copyright by crative Tim and we cannot make it public.:)
+
+
+After cloning the repository
+
+#Have react scripts and npm configured in the system
+
+## npm install -a
+## npm start 
+## configure [es lint] ((https://thomaslombart.com/setup-eslint-prettier-react/) 
+
+goto localhost:3000/ and find the react app running.
+
+Find issues and contribute for the open source. Thanks for visiting us. 
+
+Pull requests will be responded within a week time most probably.
+
+URL Of static react app deployed.
+[https://jprmohnish.github.io/PlasmaDonorhttps://jprmohnish.github.io/PlasmaDonor)
+
+Pardon for Mistakes.
---
+++ build/precache-manifest.0c46b4ec7239a9fbc8f483c4672628b5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c16196286dfe49cd5c8f84bdf9ba0fff",
+    "url": "/PlasmaDonor/index.html"
+  },
+  {
+    "revision": "0c0ead7e87c928b872d5",
+    "url": "/PlasmaDonor/static/css/2.c8df05e9.chunk.css"
+  },
+  {
+    "revision": "f71da14b7760034b2233",
+    "url": "/PlasmaDonor/static/css/main.42010d49.chunk.css"
+  },
+  {
+    "revision": "0c0ead7e87c928b872d5",
+    "url": "/PlasmaDonor/static/js/2.38641d78.chunk.js"
+  },
+  {
+    "revision": "f93d91195706ef96b5ed492769112adb",
+    "url": "/PlasmaDonor/static/js/2.38641d78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f71da14b7760034b2233",
+    "url": "/PlasmaDonor/static/js/main.2826247a.chunk.js"
+  },
+  {
+    "revision": "ad9bb1e9e75e151e7096",
+    "url": "/PlasmaDonor/static/js/runtime-main.4cb3193b.js"
+  },
+  {
+    "revision": "bfc688ca490f0079051aea5b8ec75cbe",
+    "url": "/PlasmaDonor/static/media/ayo-ogunseinde-2.bfc688ca.jpg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/PlasmaDonor/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/PlasmaDonor/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/PlasmaDonor/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/PlasmaDonor/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "7907591946eee451e53d97d92cc238c9",
+    "url": "/PlasmaDonor/static/media/react-logo.79075919.png"
+  }
+]);
